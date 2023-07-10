1477831604dba48f9f080f24d4a6b4a239aba5fa
Chirag-360 <107528552+Chirag-360@users.noreply.github.com>
2023-07-10 17:33:55 +0530

    Fetch the Games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup, GridItem, Show } from "@chakra-ui/react";
 import { Grid } from "@chakra-ui/react";
 import { Navbar } from "./components/Navbar";
+import { GameGrid } from "./components/GameGrid";
 function App() {
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
-          Main
+          <GameGrid/> 
         </GridItem>
       </Grid>
     </>
